2be89ba34dd17d15ac28b7df99414eabc7d69549
Aaron Groch <ar.groch@gmail.com>
2013-11-11 11:37:11 -0500

    removed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-b = Blog.new
-b.title = "Time is an Illusion"
-b.content = "So much content, so little time."
-b.save
-
-b = Blog.new
-b.title = "Lunchtime, Doubly So"
-b.content = "Oh, man. Don't even start talking about lunch."
-b.save
-
-b = Blog.new
-b.title = "Heart of Gold"
-b.content = "The answer to life, the universe, and everything is..."
-b.save
